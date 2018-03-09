dc979b11bddaadc0b91e72d65d59ef9d7f8a5071
Toni Lehtinen <tolehtin@gmail.com>
2018-03-09 22:40:50 +0200

    Comments

--- src/app/ngfm/connectors/ngfm-connector.ts
+++ src/app/ngfm/connectors/ngfm-connector.ts
@@ -6,6 +6,11 @@ export abstract class NgfmConnector {
   abstract mkDir(folder: NgfmFolder): Observable<NgfmFolder>
   abstract rmDir(folder: NgfmFolder): Observable<NgfmFolder>
   abstract rm(file: NgfmFile): Observable<NgfmFile>
+  /**
+   * 
+   * @param file The NgfmFile to upload
+   * @Returns Observable of the progress between 0-1. Completes when it's done.
+   */
   abstract uploadFile(file: NgfmFile): Observable<number>
   abstract folderExists(folder: NgfmFolder): Observable<boolean>
   abstract fileExists(file: NgfmFile): Observable<boolean>
